ec8503fb01b3971a2266bf5d90ae50e676053411
drusso12234 <drusso12234@gmail.com>
2020-01-09 17:27:19 -0600

    Add files via upload
    Bash script to convert the epoch time in the data files to human readable time.

---
+++ resources/translate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#To use, run script with the file you want to convert as the argument. This will replace the text in the file,
+#so if you want to keep the original as well, create a backup
+
+
+lines=$(wc -l < $1)
+echo $lines
+for ((i = 2 ; i < $lines ; i++));
+do
+    epoch=$(sed "${i}q;d" $1 | cut -c 1-10)
+    d=$(date -u -d @"$epoch")
+    sed -r -i "${i}s/([0-9]{10})/$d/g" $1
+done
