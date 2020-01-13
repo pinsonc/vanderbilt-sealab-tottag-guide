7f05e1aa5194cc40e94c0a3e2d6c83e47f5141fe
drusso12234 <drusso12234@gmail.com>
2020-01-12 20:04:44 -0600

    Add files via upload
    added skip detection for times

--- resources/translate.sh
+++ resources/translate.sh
@@ -3,12 +3,20 @@
 #To use, run script with the file you want to convert as the argument. This will replace the text in the file,
 #so if you want to keep the original as well, create a backup
 
-
 lines=$(wc -l < $1)
 echo $lines
 for ((i = 2 ; i < $lines ; i++));
 do
     epoch=$(sed "${i}q;d" $1 | cut -c 1-10)
-    d=$(date -u -d @"$epoch")
-    sed -r -i "${i}s/([0-9]{10})/$d/g" $1
+    if [[ ${epoch::1} != "#" ]] || [ $epoch!=#* ]
+    then
+	diff=$((epoch-last))
+	if [ $diff -gt 120 ] || [ $diff -lt 0 ] && [ $i != 2 ]
+	then
+     	   echo Jump on line: $i, diff is: $diff
+	fi
+	last=$epoch
+	d=$(date -u -d @"$epoch")
+	sed -r -i "${i}s/([0-9]{10})/$d/g" $1
+    fi
 done
