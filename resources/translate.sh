4b35df379921be5a88867f3a42d3c095b11c4e89
drusso12234 <drusso12234@gmail.com>
2020-01-14 09:51:28 -0600

    added more documentation and instructions in the script

--- resources/translate.sh
+++ resources/translate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#This is a bash script, it will only run in ubuntu
+#to turn this into an executable, run sudo chmod 777 translate.sh
 #To use, run script with the file you want to convert as the argument. This will replace the text in the file,
 #so if you want to keep the original as well, create a backup
 
